e83885ad3a3bfef1e2adb2fee26721651e9a5f80
Avinash <avinash.sv9@gmail.com>
2024-08-26 22:50:43 +0530

    folder creation

---
+++ PageObject/searchBar.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+class SearchBox:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def clickAndEnterText(self):
+        pass
+
+    def clickOnSearchIcon(self):
+        pass
